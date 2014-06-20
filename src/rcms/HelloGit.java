68fcc5e64b7b04523892fe018a065acd00e8ab60
xinlx.liu <xinlx.liu@qunar.com>
2014-06-20 18:45:40 +0800

    master 更改

--- src/rcms/HelloGit.java
+++ src/rcms/HelloGit.java
@@ -3,9 +3,7 @@ package rcms;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
-		System.out.println("aaaa");
-		System.out.println("study");
+		System.out.println("hello git master");
 	}
 
 }
