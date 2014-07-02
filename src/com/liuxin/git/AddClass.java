f3c43b57fc37fa6f9f2612c48fdb9e72620fe5a0
xinlx.liu <xinlx.liu@qunar.com>
2014-07-02 23:10:56 +0800

    长传

---
+++ src/com/liuxin/git/AddClass.java
@@ -0,0 +1,10 @@
+package com.liuxin.git;
+
+public class AddClass {
+
+	public static void main(String[] args) {
+		System.out.println("创建新类");
+
+	}
+
+}
--- src/rcms/HelloGit.java
+++ src/rcms/HelloGit.java
@@ -5,5 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git master");
 		System.out.println("hello git 11011");
+		int i=0;
+		while(i++<10){
+			System.out.println(i);
+		}
 	}
 }
