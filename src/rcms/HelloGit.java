b299ead2f6396c37570d50e099843c3608c55b13
xinlx.liu <xinlx.liu@qunar.com>
2014-07-16 19:12:46 +0800

    提交1

--- src/rcms/HelloGit.java
+++ src/rcms/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git master");
 		System.out.println("hello git 11011");
+
+		System.out.println("1234");
 	}
 }
